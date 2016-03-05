19411d81475d8d4c5019de4467bd7e8e90f4c2a9
Chris Tetreault <chris@doingmyprogramming.com>
2016-03-04 17:02:05 -0800

    initial freeglut/opengl/glew/cpp project

---
+++ cpp/freeglut-opengl/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
+
+int main(int argc, char ** argv)
+{
+  std::cout << "Hello world!" << std::endl;
+}
